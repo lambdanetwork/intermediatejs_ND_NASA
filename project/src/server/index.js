a9946d3394ebbdba2c96166772d05bd8f9a7f145
alfredov@garena.com <alfredov@garena.com>
2020-12-07 00:47:48 +0700

    adding server's API get images by rovername

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -25,4 +25,21 @@ app.get('/apod', async (req, res) => {
     }
 })
 
+app.get('/rover', async (req, res) => {
+    try {
+        const roverName = req.query.rover_name.toLowerCase();
+        if(!roverName) {
+            return res.status(400).end();
+        }
+        const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&camera=fhaz&api_key=${process.env.API_KEY}`
+        console.log(roverName)
+        console.log(url)
+        let image = await fetch(url)
+            .then(res => res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
