367a83f6dd307a7cad23e46b0816d207708859c5
alfredov@garena.com <alfredov@garena.com>
2020-12-07 08:44:41 +0700

    use async/await for fetch API

--- project/src/public/services/api.js
+++ project/src/public/services/api.js
@@ -1,20 +1,18 @@
 import { getStore, updateStore } from '../store/store.js';
 
-export const getImageOfTheDay = (state) => {
+export const getImageOfTheDay = async (state) => {
     let { apod } = state
     const store = getStore();
 
-    fetch(`http://localhost:3000/apod`)
+    return await fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
-
-    // return data
 }
-export const getImagesByRovername = (roverName) => {
+export const getImagesByRovername = async (roverName) => {
     const store = getStore();
 
     const roverImages = {...store.roverImages};
-    fetch(`http://localhost:3000/rover?rover_name=${roverName}`)
+    return await fetch(`http://localhost:3000/rover?rover_name=${roverName}`)
         .then(res => res.json())
         .then(data => {
             roverImages[roverName.toLowerCase()] = data.images || [];
