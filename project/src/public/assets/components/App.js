223e070b7ac1efc409c94283342a13d23b4b19cb
alfredov@garena.com <alfredov@garena.com>
2020-12-10 14:12:04 +0700

    Break APP.js to header,main,footer and refactor the component accordingly

--- project/src/public/assets/components/App.js
+++ project/src/public/assets/components/App.js
@@ -1,47 +1,37 @@
-import { getStore, updateStore } from '../../store/store.js';
+import { getStore } from '../../store/store.js';
 import { getImagesByRovername } from '../../services/api.js';
 import { useEffect } from '../../utils/useEffect.js';
 
 // components
-import { Greeting } from './Greeting.js'
-import { ImageOfTheDay } from './ImageOfTheDay.js'
+import { Apod } from './Apod.js'
+import { RoverDetail } from './RoverDetail.js'
+import { RoverSelector } from './RoverSelector.js'
+import { Tile } from './Tile.js'
+import { Header } from './Header.js'
 
 // create content
 export const App = (state) => {
-    let { rovers, apod } = state;
+    let { rovers } = state;
     const store = getStore();
+    const {roverSelected, pageSelected} = store;
     
     useEffect('App', () => {
-        const selectRoverElem = document.getElementById('rover_select');
-        selectRoverElem.onchange = function onRoverChange (e) {
-            const roverName = e.target.value;
-            getImagesByRovername(roverName)
-        }
+        getImagesByRovername(roverSelected);
     }, []);
-    
+
+    const selectedRover =  store.roverDetail[roverSelected.toLowerCase()];
     return `
-        <section>
-            <select id='rover_select'>
-                ${rovers.map(roverName => `<option value=${roverName}>${roverName}</option>`)}
-            </select>
-        </section>
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
+        ${Header()}
+        ${(pageSelected === 'rover') ?
+            `
+                <div class='main'>
+                    ${RoverSelector(rovers, roverSelected)}
+                    ${RoverDetail(selectedRover)}
+                    ${Tile(selectedRover.photos)}
+                </div>
+            `
+            : 
+            Apod()
+        }
     `
 }
