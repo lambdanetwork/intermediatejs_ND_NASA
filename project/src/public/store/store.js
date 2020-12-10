1ffb6a62dfa3876bd7eafa79c7bf2de5c8bf5512
alfredov@garena.com <alfredov@garena.com>
2020-12-10 14:13:54 +0700

    adding type for store.js

--- project/src/public/store/store.js
+++ project/src/public/store/store.js
@@ -1,19 +1,33 @@
 import { render } from '../client.js'
 
+class PhotoManifest {
+    name='';
+    landing_date='';
+    launch_date='';
+    status='';
+    max_date='';
+    max_sol='';
+    photos=[]; // string[]
+    total_photos=0;
+}
+
 let store = {
+    pageSelected: 'rover', // 'rover' | 'apod'
+    roverSelected: 'Curiosity',
     user: { name: "Student" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-    roverImages: {
-        curiosity: [],
-        opportunity: [],
-        spirit: []
+    roverDetail: {
+        curiosity: new PhotoManifest(),
+        opportunity:new PhotoManifest(),
+        spirit: new PhotoManifest()
     }
 }
 
+
 export const getStore = () => store;
 
-export const updateStore = (store, newState) => {
+export const updateStore = (newState) => {
     // make it immutable
     store = Object.assign({}, store, newState); 
     render(root, store);
