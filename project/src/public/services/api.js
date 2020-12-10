1bdca9e6164a0633dcd960b71cb9cadc57f4df78
alfredov@garena.com <alfredov@garena.com>
2020-12-10 14:11:50 +0700

    1.improve api.js 2. add try/catch

--- project/src/public/services/api.js
+++ project/src/public/services/api.js
@@ -4,18 +4,22 @@ export const getImageOfTheDay = async (state) => {
     let { apod } = state
     const store = getStore();
 
-    return await fetch(`http://localhost:3000/apod`)
+    try {
+        await fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(apod => updateStore( { apod }))
+    } catch(err){}
 }
 export const getImagesByRovername = async (roverName) => {
     const store = getStore();
+    const roverDetail = {...store.roverDetail};
+    try {
+        return await fetch(`http://localhost:3000/rover?rover_name=${roverName}`)
+            .then(res => res.json())
+            .then(data => {
+                roverDetail[roverName.toLowerCase()] = data.photo_manifest || {};
+                updateStore( { roverDetail })
+            })
+    } catch(err){}
 
-    const roverImages = {...store.roverImages};
-    return await fetch(`http://localhost:3000/rover?rover_name=${roverName}`)
-        .then(res => res.json())
-        .then(data => {
-            roverImages[roverName.toLowerCase()] = data.images || [];
-            updateStore(store, { roverImages })
-        })
 }
